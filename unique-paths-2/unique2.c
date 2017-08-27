50f331077d42d79e6fa67969a265c647de0da617
Daniel Bruce <dbruce1126@gmail.com>
2017-08-27 12:54:34 -0400

    Cleans up readability.

--- unique-paths-2/unique2.c
+++ unique-paths-2/unique2.c
@@ -1,37 +1,35 @@
 int uniquePathsWithObstacles(int **obstacleGrid, int obstacleGridRowSize, int obstacleGridColSize)
 {
-    if (obstacleGridRowSize <= 0 || obstacleGridColSize <= 0)
+    if (obstacleGridRowSize <= 0 || obstacleGridColSize <= 0 || obstacleGrid[0][0])
     {
         return 0;
     }
 
     int grid[obstacleGridRowSize][obstacleGridColSize];
     unsigned short i, j;
-    for (i = 0; i < obstacleGridRowSize; i++)
+    grid[0][0] = 1 - obstacleGrid[0][0];
+    // initialize the first column
+    for (i = 1; i < obstacleGridRowSize; i++)
     {
-        for (j = 0; j < obstacleGridColSize; j++)
+        grid[i][0] = obstacleGrid[i][0] ? 0 : grid[i - 1][0];
+    }
+    // initialize the first row
+    for (j = 1; j < obstacleGridColSize; j++)
+    {
+        grid[0][j] = obstacleGrid[0][j] ? 0 : grid[0][j - 1];
+    }
+
+    for (i = 1; i < obstacleGridRowSize; i++)
+    {
+        for (j = 1; j < obstacleGridColSize; j++)
         {
             if (obstacleGrid[i][j])
             {
                 // if this is an obstructed cell, we cannot reach this cell
                 grid[i][j] = 0;
+                continue;
             }
-            else if (i == 0)
-            {
-                // first row
-                grid[0][j] = (j == 0) ? 1 : grid[0][j - 1];
-            }
-            else if (j == 0)
-            {
-                // first column
-                grid[i][0] = grid[i - 1][0];
-            }
-            else
-            {
-                // otherwise we can reach this cell by the sum of ways we can
-                // reach the two adjacent cells (above and to our left)
-                grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
-            }
+            grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
         }
     }
     return grid[obstacleGridRowSize - 1][obstacleGridColSize - 1];
