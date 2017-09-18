8ec53e54e231ff5c7764ed5331783ad53687a085
Daniel Bruce <dbruce1126@gmail.com>
2017-09-17 21:58:55 -0400

    Adds a solution for longest palindromic substring.

---
+++ longest-palindrome-substring/README.md
@@ -0,0 +1,15 @@
+# Longest Palindromic Substring
+
+From: [leetcode.com](https://leetcode.com/problems/longest-palindromic-substring/description/)
+
+Given a string `s`, find the longest palindromic substring in `s`. You may assume that the maximum length of `s` is `1000`.
+
+Example:
+    Input: "babad"
+    Output: "bab"
+
+Note: `"aba"` is also a valid answer.
+
+Example:
+    Input: "cbbd"
+    Output: "bb"
---
+++ longest-palindrome-substring/solution.js
@@ -0,0 +1,32 @@
+const longestPalindrome = (s) => {
+  // handle the single char string or empty string
+  if (s.length < 2) return s;
+
+  // indicates the start and end of the longest palindrome
+  let start = 0;
+  let end = 0;
+  for (let i = 0; i < s.length - 1; i++) {
+    let left = i - 1; // initialize the left walker just to the left of i
+    let right = i + 1; // initialize the right walker just to the right of i
+    // walk the right walker to the end of the repeated sequence of characters
+    while (right < s.length && s[i] === s[right] && right < s.length) {
+      right++;
+    }
+    // walk the palindrome on both sides of the centre point until we no longer match
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+    // replace the longest known sequence if our current sequence is longer
+    if ((right - left - 1) > (end - start)) {
+      start = left + 1;
+      end = right - 1;
+    }
+  }
+  // return the final answer
+  return s.substr(start, end - start + 1);
+};
+
+console.log(longestPalindrome('babcd'));
+// console.log(longestPalindrome('babaddtattarrattatddetartrateedredividerb'));
+// console.log(longestPalindrome('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'));
